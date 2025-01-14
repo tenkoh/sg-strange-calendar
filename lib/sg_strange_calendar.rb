b569d0c61fd13009e93109ad7aee272a530ce301
tenkoh <tenkoh.go@gmail.com>
2025-01-14 19:54:30 +0000

    リファクタ

--- lib/sg_strange_calendar.rb
+++ lib/sg_strange_calendar.rb
@@ -5,9 +5,10 @@ class SgStrangeCalendar
   # 1月の、日曜日を起点として4日目であればCalendarPos.new(1,4)と表す(1-index)
   CalendarPos = Struct.new(:month, :dayCount)
 
+  WEEKDAYS = %w[Su Mo Tu We Th Fr Sa]
+  MONTHS = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec]
+
   def initialize(year, today = nil)
-    @weekdays = %w[Su Mo Tu We Th Fr Sa]
-    @months = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec]
     @year = year
     # カレンダー上の配置をキー、Dateオブジェクトを値としたハッシュ
     # rawCalendar[CalendarPos(1,10)]でDateオブジェクトを取得できる
@@ -29,31 +30,27 @@ class SgStrangeCalendar
   end
 
   def vertical_header
-    "#{@year} #{@months.join(" ")}"
+    "#{@year} #{MONTHS.join(" ")}"
   end
 
   # カレンダーに表示する曜日(文字列)を全て収めた配列を返す
   def all_weekdays
-    weekdays = @weekdays
-    weekdays * 5 + weekdays[0, 2]
+    WEEKDAYS * 5 + WEEKDAYS[0, 2]
   end
 
   def generate(vertical: false)
-    return generate_vertical if vertical
-    generate_horizontal
+    vertical ? generate_vertical : generate_horizontal
   end
 
   def generate_vertical
     calendar = vertical_header
     calendar << "\n"
     lines = all_weekdays.map.with_index(1) do | weekday, index |
-      line = "" # メモ：line = weekday とすると変数が使いまわされて意図せぬ出力になった
-      line << weekday
-      line << "    " # 曜日ラベルと日付の間のスペース
-      line << (1..12).map do | month |
-        date = @raw_calendar[CalendarPos.new(month, index)]
-        @prettyPrinter.print(date)
-      end.join("  ")
+      line = "#{weekday}    #{
+        (1..12).map do | month |
+          date = @raw_calendar[CalendarPos.new(month, index)]
+          @prettyPrinter.print(date)
+        end.join("  ")}"
       adjust_space(line)
     end
     calendar << lines.join("\n")
@@ -65,12 +62,11 @@ class SgStrangeCalendar
     calendar << "\n"
     lines = (1..12).map do | month |
       month_index = month - 1
-      line = @months[month_index]
-      line << "  "
-      line << all_weekdays.map.with_index(1) do | _, index |
-        date = @raw_calendar[CalendarPos.new(month, index)]
-        @prettyPrinter.print(date)
-      end.join(" ")
+      line = "#{MONTHS[month_index]}  #{
+        all_weekdays.map.with_index(1) do | _, index |
+          date = @raw_calendar[CalendarPos.new(month, index)]
+          @prettyPrinter.print(date)
+        end.join(" ")}"
       adjust_space(line)
     end
     calendar << lines.join("\n")
